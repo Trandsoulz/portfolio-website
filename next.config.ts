8fad89d1f40ca90dd5693851cf58a45567fbfbe8
Bethrand Nnaemeka <bethrand2019@gmail.com>
2026-02-28 15:48:37 +0100

    [CHORE]: Add 'framer-motion' to optimizePackageImports in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ const nextConfig: NextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   experimental: {
-    optimizePackageImports: ['react-icons'],
+    optimizePackageImports: ['framer-motion', 'react-icons'],
   },
 };
 
